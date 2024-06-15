3dcf44f032cb79650ccc686ac9a5ccf662fae32a
Michael Geers <michael@geers.tv>
2024-06-15 22:19:27 +0200

    text, meta

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "The Tractor Store";
-export const SITE_DESCRIPTION =
-  "a non-trivial micro frontends example project";
+export const SITE_TITLE = "Tractor Store 2.0 - TodoMVC for Micro Frontends";
+export const SITE_DESCRIPTION = "a non-trivial micro frontends example project";
 export const MY_NAME = "Michael Geers";
 
 // setup in astro.config.mjs
